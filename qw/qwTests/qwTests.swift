0e5c43dc745e61395c7c0797eaa07bf6a218a4af
senthilnayagam <senthil@muonium.ai>
2026-01-10 22:29:21 +0530

    Initial project setup with SwiftUI and SwiftData integration. Added main application structure, content view, item model, and basic UI tests. Included asset catalog for app icon and accent color. Configured project settings and workspace files for Xcode compatibility.

---
+++ qw/qwTests/qwTests.swift
@@ -0,0 +1,16 @@
+//
+//  qwTests.swift
+//  qwTests
+//
+//  Created by Senthil Nayagam on 10/01/26.
+//
+
+import Testing
+
+struct qwTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
